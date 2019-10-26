bd2130bddf6aef98e7031f04986b10d816bfb443
poori <pooria.poorsarvi.tehrani@mail.utoronto.ca>
2019-10-26 15:21:45 -0400

    INIT

---
+++ phase1/desktop/src/com/retromania/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.retromania.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.retromania.game.Retromania;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Retromania(), config);
+	}
+}
