4125f20b9f117d98b85de595c2f5bac146ae29fb
targuli1 <umid.targuliyev@mail.utoronto.ca>
2019-10-30 20:26:44 -0400

    Pushing modified "UFO" and "UfoManager" individuals

--- phase1/core/src/com/retromania/game/spaceship_shooter/individuals/UFO.java
+++ phase1/core/src/com/retromania/game/spaceship_shooter/individuals/UFO.java
@@ -1,4 +1,50 @@
 package com.retromania.game.spaceship_shooter.individuals;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+
 public class UFO {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    static Texture texture = new Texture("ufo.png");
+
+    public UFO(int x, int y){
+        this.x = x - Gdx.graphics.getWidth()/8;
+        this.y = y;
+        width = Gdx.graphics.getWidth()/8;
+        height = Gdx.graphics.getHeight()/20;
+
+    }
+
+    public void moveRight(){
+        if (this.x + width/2 >= Gdx.graphics.getWidth())
+            recreate();
+
+        this.x += 20;
+    }
+
+
+
+    public void draw(Batch batch, float parentAlpha) {
+        batch.draw(texture, x-width/2, y-height/2, width, height);
+    }
+
+
+    public boolean isRocketTouches(Rocket rocket){
+        double d;
+        double dp = Math.sqrt(Math.pow(x-rocket.getX(),2)+ Math.pow(y-rocket.getY(),2));
+        if (x -width/2 < rocket.getX()&& rocket.getX() < x+ width/2)
+            d = height/2 + rocket.getHeight()/2;
+        else
+            d = width/2 + rocket.getWidth()/2;
+        return d >= dp;
+    }
+
+    public void recreate(){
+        x =(int) (Math.random() * 8)*-10;
+        y = (int) (Math.random() * (Gdx.graphics.getHeight()/2 + 1) + Gdx.graphics.getHeight()/4);
+    }
 }
--- phase1/core/src/com/retromania/game/spaceship_shooter/individuals/UfoManager.java
+++ phase1/core/src/com/retromania/game/spaceship_shooter/individuals/UfoManager.java
@@ -1,4 +1,47 @@
 package com.retromania.game.spaceship_shooter.individuals;
 
-public class UfoManager {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UfoManager extends Actor {
+    int height;
+    private int numOfUfos;
+    private List<UFO> UFOList;
+
+    public UfoManager(int numOfUfos){
+        this.numOfUfos = numOfUfos;
+        UFOList = new ArrayList<UFO>();
+        height = Gdx.graphics.getHeight();
+
+        double tempy;
+        for(int i = 0; i< numOfUfos; i++){
+            tempy= Math.random() * (height/2 + 1) + height/4;
+            UFOList.add(new UFO(-200*i,(int) tempy));
+
+        }
+    }
+
+    public void update(Rocket rocket, Hud hud){
+        for (UFO i : UFOList)
+            i.moveRight();
+
+        if (rocket != null && !rocket.reach_top() ){
+            for (UFO i: UFOList){
+                if(i.isRocketTouches(rocket)){
+                    hud.addScore(10);
+                    i.recreate();
+                }
+            }
+        }
+
+    }
+
+    public void draw(Batch batch, float delta){
+        for (UFO i: UFOList)
+            i.draw(batch, delta);
+    }
 }
