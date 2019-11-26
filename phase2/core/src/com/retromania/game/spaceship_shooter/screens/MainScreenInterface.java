3e1649058938d15a29cf3b99a90d2513600d8021
thuy.le <ctoassistant@smacinnovation.com>
2019-11-25 20:47:09 -0500

    ADDED SPACESHIP SHOOTER STEP 10-11: Change the implementation of classes in screens folder and add java docs, comments.

--- phase2/core/src/com/retromania/game/spaceship_shooter/screens/MainScreenInterface.java
+++ phase2/core/src/com/retromania/game/spaceship_shooter/screens/MainScreenInterface.java
@@ -2,10 +2,23 @@ package com.retromania.game.spaceship_shooter.screens;
 
 import com.retromania.game.shared_abstractions.User;
 
+/**
+ * The MainScreenInterface is created to reduce coupling between
+ * the Screen and its subclasses.
+ */
 public interface MainScreenInterface {
+    // pause the screen
     public void pause();
+
+    // resume the screen
     public void resume();
+
+    // restart the screen
     public void restart();
+
+    // get the user of screen.
     public User getUser();
+
+    //save the screen.
     public void save(Object... args);
 }
