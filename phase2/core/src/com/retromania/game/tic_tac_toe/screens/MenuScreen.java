85773f750ef91ebaee2108ca55bc70b371f39577
Hyokyung Kim <hyokyung.kim@mail.utoronto.ca>
2019-11-28 15:38:07 -0500

    ADDED TICTACTOE STEP 13: Added option to play with cat icons

--- phase2/core/src/com/retromania/game/tic_tac_toe/screens/MenuScreen.java
+++ phase2/core/src/com/retromania/game/tic_tac_toe/screens/MenuScreen.java
@@ -1,6 +1,7 @@
 package com.retromania.game.tic_tac_toe.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -8,11 +9,17 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.retromania.game.shared_abstractions.RetroManiaScreen;
 
@@ -25,43 +32,77 @@ public class MenuScreen extends RetroManiaScreen {
     public float gameWidth;
     public float gameHeight;
     ImageButton playButton;
+    ImageButton catButton;
     Viewport viewport;
+    Skin uiSkin;
+    boolean cats = false;
+    private Label outputLabel;
 
     public MenuScreen(){
         gamecam = new OrthographicCamera();
         gameWidth = Gdx.graphics.getWidth();
         gameHeight = Gdx.graphics.getHeight();
-        viewport = new FitViewport(gameWidth, gameHeight, gamecam);
+        viewport = new FillViewport(gameWidth, gameHeight, gamecam);
         stage = new Stage(viewport, game.sb);
         Gdx.input.setInputProcessor(stage);
         batch = new SpriteBatch();
         Table table = new Table();
         table.setBounds(0, 0, gameWidth, gameHeight);
+//        uiSkin = new Skin(Gdx.files.internal("tic_tac_toe/uiskin.json"));
         playButton = new ImageButton( new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("tic_tac_toe/play_tictactoe.png")))));
-        playButton.setSize(500,500);
-        playButton.setPosition(gameWidth/2 - 250, gameHeight/2-200);
+        playButton.setSize(200,200);
+        playButton.setPosition(gameWidth/2 - 110, gameHeight/2-70);
         stage.addActor(playButton);
+
+        /**Create cat buttons, tutorial from "https://alvinalexander.com/source-code/how-create-libgdx-scene2d-imagebutton" */
+        Texture catTexture = new Texture(Gdx.files.internal("tic_tac_toe/radio-off-button.png"));
+        Texture catTexturePressed = new Texture(Gdx.files.internal("tic_tac_toe/radio-on-button.png"));
+        catButton = new ImageButton(
+                new TextureRegionDrawable(new TextureRegion(catTexture)),
+                new TextureRegionDrawable(new TextureRegion(catTexturePressed)),
+                new TextureRegionDrawable(new TextureRegion(catTexturePressed))
+        );
+        catButton.setSize(60, 60);
+        catButton.setPosition(gameWidth/2-220, gameHeight/3);
+
+        catButton.addListener(
+                new InputListener() {
+                    @Override
+                    public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                        System.out.println("CATS PRESSED");
+                        if (cats == false) {
+                            catButton.setChecked(true);
+                            cats = true;
+                        }
+                        else{
+                            catButton.setChecked(false);
+                            cats = false;
+                        }
+                    }
+
+                    @Override
+                    public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                        System.out.println("CATS...");
+                        return true;
+                    }
+                });
+        stage.addActor(catButton);
+
+        stage.addActor(table);
+        table.debug();
 
     }
 
     @Override
     public void handleInput() {
-        if (Gdx.input.isTouched()) {
-            Vector3 mousePos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
-            System.out.println(mousePos);
-            Vector3 worldCoordinates = gamecam.unproject(mousePos);
-            if (worldCoordinates.x >= gameWidth/2 - 250 && worldCoordinates.x <= gameWidth/2 +250){
-                if (worldCoordinates.y >= gameHeight/2-200 && worldCoordinates.y <=gameWidth/2+300){
-                    System.out.println("CLICKED!!!");
-                    game.setScreen(new PlayScreen());
-                }
-            }
+        if (playButton.isPressed()){
+            game.setScreen(new PlayScreen(cats));
         }
     }
 
     @Override
     public void show() {
-
+        Gdx.input.setInputProcessor(stage);
 
     }
 
@@ -73,6 +114,13 @@ public class MenuScreen extends RetroManiaScreen {
         stage.act(delta);
         game.sb.setProjectionMatrix(stage.getCamera().combined);
         stage.draw();
+        batch.setProjectionMatrix(gamecam.combined);
+        batch.begin();
+        font.setColor(Color.BLACK);
+        font.getData().setScale(2, 2);
+        font.draw(batch, "CATS!", gameWidth/2-210, gameHeight/3-30);
+        batch.end();
+
     }
 
     @Override
