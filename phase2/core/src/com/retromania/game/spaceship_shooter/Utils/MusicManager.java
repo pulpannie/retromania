8560c49d5c7c51a42e7f2e8a8d5be8a94af2bb93
targuli1 <umid.targuliyev@mail.utoronto.ca>
2019-11-30 22:22:48 -0500

    ADDED SPACESHIP SHOOTER STEP 46: added javadocs and comments, also reformatted code in MusicManager and SpaceShipShooterStarter

--- phase2/core/src/com/retromania/game/spaceship_shooter/Utils/MusicManager.java
+++ phase2/core/src/com/retromania/game/spaceship_shooter/Utils/MusicManager.java
@@ -3,11 +3,31 @@ package com.retromania.game.spaceship_shooter.Utils;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 
+/**
+ * class that controls music of the game
+ *
+ * @author Umid, Thuy
+ */
 public class MusicManager {
-    private static Music music = Gdx.audio.newMusic(Gdx.files.internal("spaceship_shooter/shooting_star_music.mp3"));
-    public MusicManager(){
-    }
-    public static void pause(){music.pause();}
-    public static void play(){music.play();}
-    public static void stop(){music.stop();}
+  /** instance of music */
+  private static Music music =
+      Gdx.audio.newMusic(Gdx.files.internal("spaceship_shooter/shooting_star_music.mp3"));
+
+  /** Constructor of MusicManager */
+  public MusicManager() {}
+
+  /** pauses music */
+  public static void pause() {
+    music.pause();
+  }
+
+  /** plays music */
+  public static void play() {
+    music.play();
+  }
+
+  /** stops music */
+  public static void stop() {
+    music.stop();
+  }
 }
