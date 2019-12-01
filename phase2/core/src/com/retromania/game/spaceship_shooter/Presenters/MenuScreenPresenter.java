6446b0cad9b6016c167432836e20763577447c83
targuli1 <umid.targuliyev@mail.utoronto.ca>
2019-11-30 21:45:38 -0500

    ADDED SPACESHIP SHOOTER STEP 44-45: added javadocs and comments, also reformatted codes in Presenter package

--- phase2/core/src/com/retromania/game/spaceship_shooter/Presenters/MenuScreenPresenter.java
+++ phase2/core/src/com/retromania/game/spaceship_shooter/Presenters/MenuScreenPresenter.java
@@ -10,99 +10,95 @@ import com.retromania.game.spaceship_shooter.Views.MainScreenInterface;
 import java.util.Locale;
 
 /**
- * Presenter class of MenuScreen. It has access to models of MenuScreen and updates models by command of view class
+ * Presenter class of MenuScreen. It has access to models of MenuScreen and updates models by
+ * command of view class
  *
  * @author Umid, Thuy
  */
 public class MenuScreenPresenter extends Presenter {
-    /**
-     * Interface to give input to viewport of starter class
-     */
-    private MainScreenInterface mainScreen;
-    /**
-     * Label that shows up high score
-     */
-    private Label highScoreLabel;
-    /**
-     * Label that shows up latest score
-     */
-    private Label latestScoreLabel;
-    /**
-     * Stage that holds table of labels to print
-     */
-    private Stage stage;
+  /** Interface to give input to viewport of starter class */
+  private MainScreenInterface mainScreen;
+  /** Label that shows up high score */
+  private Label highScoreLabel;
+  /** Label that shows up latest score */
+  private Label latestScoreLabel;
+  /** Stage that holds table of labels to print */
+  private Stage stage;
 
-    /**
-     * Constructor class for MenuScreenPresenter
-     *
-     * @param mainScreen access to viewport of starter class through interface
-     */
-    public MenuScreenPresenter(MainScreenInterface mainScreen){
-        super();
-        // Label that shows up word "High Score"
-        Label highScoreTextLabel;
-        // Label that shows up word "Latest Score"
-        Label latestScoreTextLabel;
-        // Table that stores labels
-        Table table;
+  /**
+   * Constructor class for MenuScreenPresenter
+   *
+   * @param mainScreen access to viewport of starter class through interface
+   */
+  public MenuScreenPresenter(MainScreenInterface mainScreen) {
+    super();
+    // Label that shows up word "High Score"
+    Label highScoreTextLabel;
+    // Label that shows up word "Latest Score"
+    Label latestScoreTextLabel;
+    // Table that stores labels
+    Table table;
 
-        this.mainScreen = mainScreen;
+    this.mainScreen = mainScreen;
 
-        LabelBuilder labelBuilder = new LabelBuilder();
-        labelBuilder.buildFont(6.0f).buildColor().buildLabelStyle();
+    LabelBuilder labelBuilder = new LabelBuilder();
+    labelBuilder.buildFont(6.0f).buildColor().buildLabelStyle();
 
-        highScoreTextLabel = labelBuilder.buildText("High Score").buildLabel();
-        latestScoreTextLabel = labelBuilder.buildText("Latest Score").buildLabel();
-        highScoreLabel = labelBuilder.buildText(String.format(Locale.US,"%03d", SpaceShipShooterStarter.getGameStats().getHighScore())).buildLabel();
-        latestScoreLabel = labelBuilder.buildText(String.format(Locale.US,"%06d", SpaceShipShooterStarter.getGameStats().getLatestScore())).buildLabel();
+    highScoreTextLabel = labelBuilder.buildText("High Score").buildLabel();
+    latestScoreTextLabel = labelBuilder.buildText("Latest Score").buildLabel();
+    highScoreLabel =
+        labelBuilder
+            .buildText(
+                String.format(
+                    Locale.US, "%03d", SpaceShipShooterStarter.getGameStats().getHighScore()))
+            .buildLabel();
+    latestScoreLabel =
+        labelBuilder
+            .buildText(
+                String.format(
+                    Locale.US, "%06d", SpaceShipShooterStarter.getGameStats().getLatestScore()))
+            .buildLabel();
 
+    table = new Table();
+    table.top();
+    table.setFillParent(true);
+    table.add(highScoreTextLabel).expandX().pad(10);
+    table.add(highScoreLabel).expandX();
+    table.row();
+    table.add(latestScoreTextLabel).expandX();
+    table.add(latestScoreLabel).expandX().pad(10);
 
-        table = new Table();
-        table.top();
-        table.setFillParent(true);
-        table.add(highScoreTextLabel).expandX().pad(10);
-        table.add(highScoreLabel).expandX();
-        table.row();
-        table.add(latestScoreTextLabel).expandX();
-        table.add(latestScoreLabel).expandX().pad(10);
+    stage = new Stage();
+    stage.addActor(table);
+  }
 
-        stage = new Stage();
-        stage.addActor(table);
-    }
+  /** dispose the menu screen */
+  public void dispose() {}
 
-    /**
-     * dispose the menu screen
-     */
-    public void dispose() {
+  /** update the menu screen */
+  public void update(float dt) {
+    super.update(dt);
+  }
 
-    }
+  /** start the game */
+  public void start() {
+    mainScreen.restart();
+  }
 
-    /**
-     * update the menu screen
-     */
-    public void update(float dt){
-        super.update(dt);
-    }
+  /** update the highscore and the latest score. */
+  public void updateTable() {
+    highScoreLabel.setText(
+        String.format(Locale.US, "%03d", SpaceShipShooterStarter.getGameStats().getHighScore()));
+    latestScoreLabel.setText(
+        String.format(Locale.US, "%03d", SpaceShipShooterStarter.getGameStats().getLatestScore()));
+  }
 
-    /**
-     * start the game
-     */
-    public void start() {
-        mainScreen.restart();
-    }
-
-    /**
-     * update the highscore and the latest score.
-     */
-    public void updateTable(){
-        highScoreLabel.setText(String.format(Locale.US,"%03d", SpaceShipShooterStarter.getGameStats().getHighScore()));
-        latestScoreLabel.setText(String.format(Locale.US,"%03d", SpaceShipShooterStarter.getGameStats().getLatestScore()));
-    }
-
-    /**
-     * get the stage
-     * @return the stage
-     */
-    public Stage getStage(){return stage;}
-
+  /**
+   * get the stage
+   *
+   * @return the stage
+   */
+  public Stage getStage() {
+    return stage;
+  }
 }
--- phase2/core/src/com/retromania/game/spaceship_shooter/Presenters/StarterPresenter.java
+++ phase2/core/src/com/retromania/game/spaceship_shooter/Presenters/StarterPresenter.java
@@ -9,71 +9,116 @@ import com.retromania.game.spaceship_shooter.Views.StateFactory;
 import com.retromania.game.spaceship_shooter.Utils.MusicManager;
 import com.retromania.game.utils.GameSaver;
 
-
+/**
+ * Presenter class of SpaceShipShooterStarter. It has access to models of SpaceShipShooter and
+ * updates models by command of view class(SpaceShipShooterStarter)
+ *
+ * @author Umid, Thuy
+ */
 public class StarterPresenter {
-    private static GameStats gameStats;
-    private static String theme = "Independence Day";
-    private static boolean isMusic = false;
-    private Screen playScreen;
-    private Screen pauseScreen;
-    private Screen menuScreen;
-    private Screen settingScreen;
-    private GameSaver gameSaver;
+  /** class that keeps all scores, best score and latest score in current session */
+  private static GameStats gameStats;
+  /** Current theme of game */
+  private static String theme = "Independence Day";
+  /** Current state of whether music isPlayed or not */
+  private static boolean isMusic = false;
+  /** instance of play screen */
+  private Screen playScreen;
+  /** instance of pause screen */
+  private Screen pauseScreen;
+  /** instance of menu screen */
+  private Screen menuScreen;
+  /** instance of setting screen */
+  private Screen settingScreen;
+  /** class that saves our game locally */
+  private GameSaver gameSaver;
 
-    public StarterPresenter(MainScreenInterface mainScreen){
-        gameStats = new GameStats();
-        playScreen = StateFactory.getScreen("play screen", mainScreen);
-        pauseScreen = StateFactory.getScreen("pause screen", mainScreen);
-        menuScreen = StateFactory.getScreen("menu screen",  mainScreen);
-        settingScreen = StateFactory.getScreen("setting screen",  mainScreen);
-        gameSaver = new GameSaver("Spaceship shooter");
-    }
+  /**
+   * Constructor class for StarterPresenter
+   *
+   * @param mainScreen access to viewport of starter class through interface
+   */
+  public StarterPresenter(MainScreenInterface mainScreen) {
+    gameStats = new GameStats();
+    playScreen = StateFactory.getScreen("play screen", mainScreen);
+    pauseScreen = StateFactory.getScreen("pause screen", mainScreen);
+    menuScreen = StateFactory.getScreen("menu screen", mainScreen);
+    settingScreen = StateFactory.getScreen("setting screen", mainScreen);
+    gameSaver = new GameSaver("Spaceship shooter");
+  }
 
-    public void pause() {
-        RetroMania.getRetroManiaInstance().setScreen(pauseScreen);
-    }
+  /** Sets screen to Pause */
+  public void pause() {
+    RetroMania.getRetroManiaInstance().setScreen(pauseScreen);
+  }
 
-    public void resume() {
-        RetroMania.getRetroManiaInstance().setScreen(playScreen);
-    }
+  /** Sets screen to Play */
+  public void resume() {
+    RetroMania.getRetroManiaInstance().setScreen(playScreen);
+  }
 
+  /** Sets screen to Settings */
+  public void modify() {
+    RetroMania.getRetroManiaInstance().setScreen(settingScreen);
+  }
 
-    public void modify() {
-        RetroMania.getRetroManiaInstance().setScreen(settingScreen);
-    }
+  /**
+   * Sets screen to Menu
+   *
+   * @param theme chosen type of theme for game
+   * @param isMusic boolean that keeps whether music should play or not
+   */
+  public void returnMenu(String theme, boolean isMusic) {
+    StarterPresenter.theme = theme;
+    StarterPresenter.isMusic = isMusic;
+    RetroMania.getRetroManiaInstance().setScreen(menuScreen);
+  }
 
+  /** Sets screen to Menu */
+  public void returnMenu() {
+    RetroMania.getRetroManiaInstance().setScreen(menuScreen);
+  }
 
-    public void returnMenu(String theme, boolean isMusic) {
-        StarterPresenter.theme = theme;
-        StarterPresenter.isMusic = isMusic;
-        RetroMania.getRetroManiaInstance().setScreen(menuScreen);
-    }
+  /**
+   * Creates new play screen and sets screen to new play screen
+   *
+   * @param mainScreen
+   */
+  public void restart(MainScreenInterface mainScreen) {
+    playScreen = new PlayScreen(mainScreen);
+    RetroMania.getRetroManiaInstance().setScreen(playScreen);
+  }
 
-    public void returnMenu() {
-        RetroMania.getRetroManiaInstance().setScreen(menuScreen);
-    }
+  /**
+   * Getter method for theme
+   *
+   * @return theme
+   */
+  public static String getTheme() {
+    return theme;
+  }
 
-    public void restart(MainScreenInterface mainScreen){
-        playScreen = new PlayScreen(mainScreen);
-        RetroMania.getRetroManiaInstance().setScreen(playScreen);
-    }
+  /**
+   * Getter method for gameStats
+   *
+   * @return gameStats
+   */
+  public static GameStats getGameStats() {
+    return gameStats;
+  }
 
-    public static String getTheme() {
-        return theme;
-    }
+  /** Updates MusicManager according to state of music */
+  public static void playMusic() {
+    if (isMusic) MusicManager.play();
+    else MusicManager.stop();
+  }
 
-    public static GameStats getGameStats() {
-        return gameStats;
-    }
-
-    public static void playMusic() {
-        if (isMusic)
-            MusicManager.play();
-        else
-            MusicManager.stop();
-    }
-
-    public void setScore(int score){
-        gameSaver.setScore(score);
-    }
+  /**
+   * Saves score of user
+   *
+   * @param score score of user in this game
+   */
+  public void setScore(int score) {
+    gameSaver.setScore(score);
+  }
 }
