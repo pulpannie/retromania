2011b3f4bd63e3928df16e00ef8a5b658d56565d
thuy.le <ctoassistant@smacinnovation.com>
2019-11-30 20:32:30 -0500

    ADDED SPACESHIP SHOOTER STEP 42-43: Add parameter description java docs and reformatted.

--- phase2/core/src/com/retromania/game/spaceship_shooter/Models/Background.java
+++ phase2/core/src/com/retromania/game/spaceship_shooter/Models/Background.java
@@ -9,57 +9,42 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
  * Background of game, which remains unchanged during the game
  *
  * @author Umid, Thuy
- * */
-
-
+ */
 public class Background extends Actor {
-    /**
-     * renderer to draw simple objects
-     */
-    private ShapeRenderer shapeRenderer;
+  /** renderer to draw simple objects */
+  private ShapeRenderer shapeRenderer;
 
-    /**
-     * boolean for check if batch is set for projection
-     */
-    private static boolean projectionMatrixSet;
+  /** boolean for check if batch is set for projection */
+  private static boolean projectionMatrixSet;
 
-    /**
-     * width of background
-     */
-    private int width;
+  /** width of background */
+  private int width;
 
-    /**
-     * height of  background
-     */
-    private int height;
+  /** height of background */
+  private int height;
 
-    /**
-     * constructor of Background
-     * */
-    public Background(){
-        shapeRenderer = new ShapeRenderer();
-        projectionMatrixSet = false;
-        width = Gdx.graphics.getWidth();
-        height = Gdx.graphics.getHeight();
+  /** constructor of Background */
+  public Background() {
+    shapeRenderer = new ShapeRenderer();
+    projectionMatrixSet = false;
+    width = Gdx.graphics.getWidth();
+    height = Gdx.graphics.getHeight();
+  }
+
+  /** Method that draws background with shapeRenderer */
+  @Override
+  public void draw(Batch batch, float parentAlpha) {
+    batch.end();
+    if (!projectionMatrixSet) {
+      shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
     }
 
-    /**
-     * Method that draws background with shapeRenderer
-     */
-    @Override
-    public void draw(Batch batch, float parentAlpha) {
-        batch.end();
-        if(!projectionMatrixSet){
-            shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
-        }
-
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.setColor(Color.SKY);
-        shapeRenderer.rect(0, height/8, width, 7*height/8);
-        shapeRenderer.setColor(Color.CORAL);
-        shapeRenderer.rect(0,0, width, height/8);
-        shapeRenderer.end();
-        batch.begin();
-    }
-
+    shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+    shapeRenderer.setColor(Color.SKY);
+    shapeRenderer.rect(0, height / 8, width, 7 * height / 8);
+    shapeRenderer.setColor(Color.CORAL);
+    shapeRenderer.rect(0, 0, width, height / 8);
+    shapeRenderer.end();
+    batch.begin();
+  }
 }
--- phase2/core/src/com/retromania/game/spaceship_shooter/Models/GameStats.java
+++ phase2/core/src/com/retromania/game/spaceship_shooter/Models/GameStats.java
@@ -2,54 +2,47 @@ package com.retromania.game.spaceship_shooter.Models;
 
 import java.util.ArrayList;
 import java.util.List;
-/**
- * Class that controls game statistics including best and last score
- * */
+/** Class that controls game statistics including best and last score */
 public class GameStats {
 
-    /**
-     * keep track of all games played
-     */
-    private List<Integer> scoreboard;
+  /** keep track of all games played */
+  private List<Integer> scoreboard;
 
-    /**
-     * integer that scores highest score
-     */
-    private int highScore = 0;
+  /** integer that scores highest score */
+  private int highScore = 0;
 
-    /**
-     * integer that keeps track of number of times games played
-     */
-    private int timePlay = 0;
+  /** integer that keeps track of number of times games played */
+  private int timePlay = 0;
 
-    /**
-     * Initializer of GameStats class
-     * */
-    public GameStats() {
-        scoreboard = new ArrayList<>();
-    }
-    /**
-     * getter for high score
-     * @return highScore
-     * */
-    public int getHighScore() {
-        return highScore;
-    }
+  /** Initializer of GameStats class */
+  public GameStats() {
+    scoreboard = new ArrayList<>();
+  }
+  /**
+   * getter for high score
+   *
+   * @return highScore
+   */
+  public int getHighScore() {
+    return highScore;
+  }
 
-    /**
-     * Method that add new score to scoreboard and checks if it is the new high score
-     * @param score:   the score of the user.
-     */
-    public void update(int score) {
-        scoreboard.add(score);
-        timePlay++;
-        highScore = (score > highScore) ? score : highScore;
-    }
-    /**
-     * Getter for latestScore
-     * @return the latestScore
-     * */
-    public int getLatestScore() {
-        return (timePlay == 0) ? 0 : scoreboard.get(timePlay-1);
-    }
+  /**
+   * Method that add new score to scoreboard and checks if it is the new high score
+   *
+   * @param score: the score of the user.
+   */
+  public void update(int score) {
+    scoreboard.add(score);
+    timePlay++;
+    highScore = (score > highScore) ? score : highScore;
+  }
+  /**
+   * Getter for latestScore
+   *
+   * @return the latestScore
+   */
+  public int getLatestScore() {
+    return (timePlay == 0) ? 0 : scoreboard.get(timePlay - 1);
+  }
 }
